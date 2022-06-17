6c8937c54109afcbe20acfc269392b8414c0c087
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-17 19:03:55 -0300

    criando arquivo admin

---
+++ mysite/mysite/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Post, Category
+
+
+@admin.register(Category)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'criado', 'publicado')
+    list_filter = ('nome', 'criado', 'publicado')
+    date_hierarchy = 'publicado'
+    search_fields = ('nome',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'publicado', 'status')
+    list_filter = ('status', 'criado', 'publicado', 'autor')
+    readonly_fields = ('visualizar_imagem',)
+    raw_id_fields = ('autor',)
+    date_hierarchy = 'publicado'
+    search_fields = ('titulo', 'conteudo')
+    prepopulated_fields = {'slug': ('titulo',)}
+
+    def visualizar_imagem(self, obj):
+        return obj.view_image
+    visualizar_imagem.short_description = "Imagem Cadastrada"
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,16 @@
-# https://docs.djangoproject.com/en/3.2/topics/http/urls/
+from django.urls import path
 
-from django.contrib import admin
-from django.urls import include, path
+from . import views
+
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.BlogListView.as_view(), name='home'),
+    path('post/teste/', views.hello, name='hello'),
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
+    path('post/<slug:slug>/', views.BlogDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='post_delete'),
 ]
