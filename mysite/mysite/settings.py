31e4baf1bbb787605a254471499ff583c6d1d0ca
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-17 17:14:43 -0300

    criando o banco de dados

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from dotenv import load_dotenv
+import dotenv
+import os
 
 from pathlib import Path
 
@@ -22,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv()
+dotenv.load_dotenv(dotenv.find_dotenv())
+
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
