a1e847a5305de40ec072be0309a4238c4a96df43
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-17 19:40:06 -0300

    mudança de repositorio

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,16 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 
-from . import views
-
-from django.urls import path
-
-from . import views
+from pages.views import home_view
 
 urlpatterns = [
-    path('', views.BlogListView.as_view(), name='home'),
-    path('post/teste/', views.hello, name='hello'),
-    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
-    path('post/<slug:slug>/', views.BlogDetailView.as_view(), name='post_detail'),
-    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='post_edit'),
-    path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='post_delete'),
+    path('', home_view, name="home"),
+    path('/admin', admin.site.urls),
 ]
