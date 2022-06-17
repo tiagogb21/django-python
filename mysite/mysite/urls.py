19ad5a48c6a414a6f5b5b01e38b77faf9027a770
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-17 20:22:01 -0300

    configurando rotas

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from pages.views import home_view
 
 urlpatterns = [
-    path('', home_view, name="home"),
-    path('/admin', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('base.urls'))
 ]
