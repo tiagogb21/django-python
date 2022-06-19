af38183bb98772bf8d35f3f4f7451fd3d4600fcf
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-19 13:49:07 -0300

    criando a classe message na models

--- mysite/base/models.py
+++ mysite/base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # auto_now --> take a snapeshot every time we save this
+    update = models.DateTimeField(auto_now=True)
+    # auto_now_add --> only takes a timestamp when we first save or created
+    created = models.DateTimeField(auto_now_add=True)
+
+    # string representation
+    def __str__(self):
+        return str(self.name)
