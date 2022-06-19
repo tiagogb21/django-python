441d220b5e3edb693f3da45cb493af0f48221446
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-19 11:03:28 -0300

    criando para cada diretorio uma template

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
 
+rooms = [
+    {'id': 1, 'name': 'django'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend'},
+]
+
+
 def home(request):
     return render(request, 'home.html')
 
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'room.html', {'rooms': rooms})
