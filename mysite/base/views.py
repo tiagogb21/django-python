1b19980d7d5dc6efc9825a6ab7c491b8e6f73753
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-19 10:44:01 -0300

    componentizando room e home

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
 
 
-def home(requests):
-    return HttpResponse('Home page')
+def room(request):
+    return render(request, 'room.html')
